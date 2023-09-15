acd705f0847928f069cf55c35e1a79064e0fd451
Ankit Yadav <ooacj15@tataaia.com>
2023-09-15 18:38:14 +0530

    added

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const NavBar = () => {
+  let isAuth: boolean = false
+  return (
+    <div className='flex text-sm space-x-4 dark:bg-gray-600 bg-gray-200 m-2 p-2 rounded-sm'>
+      <Link href='/'>Home</Link>
+      <Link href='/dashboard'>Dashboard</Link>
+      { !isAuth && <Link href='/login'>Login</Link> }
+    </div>
+  )
+}
+
+export default NavBar

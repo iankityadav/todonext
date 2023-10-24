a14bf7cc01f0776b47c700d5a328eecf6be86a51
iankityadav <1002ankit@gmail.com>
2023-10-24 21:42:14 +0530

    home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
+"use client"
+
+import UserCard from "./components/UserCard";
+import { User } from "./models/user";
+import { useEffect, useState } from "react";
+import { useAppSelector } from "./store";
+
 export default function Home() {
+  const isAuth = useAppSelector((state) => state.auth.isAuth);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
+  let [user, setUser] = useState<User | undefined>(); //useAppSelector((state) => state.auth.user);
+  useEffect(() => {
+    setIsAuthenticated(isAuth);
+    setUser({ email: 'ankit@gmail.com', name: 'ANkit Yadav' })
+    console.log(user)
+  }, [isAuth])
   return (
-    <>
-      <div className='flex flex-col justify-center items-center p-2 h-96'>
-        <div className="text-4xl font-semibold tracking-wider">
-          Todo Application
-        </div>
+    <div className="h-screen flex justify-center items-center">
+      <div className='flex flex-col justify-center p-2 h-max'>
+        {!isAuthenticated &&
+          <div className="text-5xl font-bold tracking-tight">
+            Todo Application
+          </div>
+        }
+        {isAuthenticated && (<>
+          <div className="text-5xl font-bold tracking-tight">
+            Welcome {user?.name.split(" ")[0]}!
+          </div>
+          <div className="mt-6 flex justify-center items-center">
+            {user && <UserCard user={user} />}
+          </div>
+        </>)
+        }
       </div>
-    </>
+    </div>
   )
 }

0adaa35bb475f76e5418aadea5bde344547919ba
iankityadav <1002ankit@gmail.com>
2023-10-24 21:40:48 +0530

    nav bar updated to show buttons dynamically

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,20 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import { useAppSelector } from '../store'
 
 
 const NavBar = () => {
-  let isAuthenticated: boolean = false
+  const isAuthenticated: boolean = useAppSelector((state) => state.auth.isAuth)
   return (
-    <div className='flex text-sm space-x-4 dark:bg-gray-600 bg-gray-200 m-2 p-2 rounded-sm'>
-      <Link href='/'>Home</Link>
-      <Link href='/dashboard'>Dashboard</Link>
-      { !isAuthenticated && <Link href='/login'>Login</Link> }
-      { !isAuthenticated && <Link href='/signup'>Sign up</Link> }
+    <div className='flex justify-center'>
+      <div className='flex text-base font-light xl:space-x-16 space-x-4 dark:bg-gray-800 bg-gray-200 shadow-md dark:shadow-indigo-500 m-2 p-2 rounded-sm fixed w-11/12 justify-center'>
+        <Link href='/'>Home</Link>
+        <Link href='/dashboard'>Dashboard</Link>
+        {isAuthenticated && <Link href='/login'>Logout</Link>}
+        {!isAuthenticated && <Link href='/login'>Login</Link>}
+        {!isAuthenticated && <Link href='/signup'>Sign up</Link>}
+      </div>
     </div>
   )
 }

e9f857fc0cb13742ffc9c45c28ed7f51f0c9c7c6
iankityadav <1002ankit@gmail.com>
2023-11-15 07:30:37 +0530

    logout comp added

--- app/logout/components/Logout.tsx
+++ app/logout/components/Logout.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+"use client"
+import { useAppDispatch } from "@/app/store";
+import { logOut } from "@/app/store/auth/auth.slice"
 
 const Logout = () => {
+  const dispatch = useAppDispatch();
   return (
-    <div className="h-screen flex justify-center items-center">
-        Logout
-    </div>
+    <>
+      {
+        dispatch(logOut()) && < div className="h-screen flex justify-center items-center" >
+          Logged out Succesfully
+        </div >
+      }
+    </>
   )
 }
 

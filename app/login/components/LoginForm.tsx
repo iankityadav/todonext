6aa4763ddad4b75c8dfb5b292edb8b7194c73f5e
iankityadav <1002ankit@gmail.com>
2023-11-15 09:37:03 +0530

    failure alert

--- app/login/components/LoginForm.tsx
+++ app/login/components/LoginForm.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch, useAppSelector } from "@/app/store";
 import { logIn } from "@/app/store/auth/auth.slice";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const LoginForm = () => {
     const isAuth = useAppSelector((state) => state.auth.isAuth)
@@ -29,7 +30,20 @@ const LoginForm = () => {
             })
             const data = await res.json()
             console.log(data);
-            dispatch(logIn(data?.token))
+            if (res.ok) {
+                dispatch(logIn(data?.token))
+            } else {
+                Swal.fire({
+                    // title: "Failed to login",
+                    icon: 'error',
+                    text: 'Please check your email or passowrd.',
+                    timer: 5000,
+                    showCloseButton: true,
+                    customClass: {
+                        popup: "dark:bg-gray-700 dark:text-white text-black flex"
+                    }
+                })
+            }
         } catch (error) {
             console.error(error);
         }
@@ -37,7 +51,7 @@ const LoginForm = () => {
 
     return (
         <section className="">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -65,7 +79,7 @@ const LoginForm = () => {
                             </div>
                             <button type="submit" className="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don't have an account yet? <a href="#" className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Sign up</a>
+                                Don't have an account yet? <a href="/signup" className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Sign up</a>
                             </p>
                         </form>
                     </div>
